bbd1c63fe91e9b0b7eb07a133bc44708e986932c
wuct <wu.chingting@gmail.com>
2015-01-18 21:00:32 +0800

    use websocket

--- client/client.js
+++ client/client.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
-var io = require('socket.io-client');
 var ss = require('socket.io-stream');
 
 var SERVER_URL = process.env.SERVER_URL || 'http://local.host:3000';
-var socket = io(SERVER_URL + '/stream', { query: "type=client" });
+var socket = require('socket.io-client')(SERVER_URL + '/stream', {
+		query: "type=client",
+		transports: ['websocket']
+});
 
 socket.on('connect_error', function(){
 	console.log('Connection Failed');
@@ -12,15 +14,16 @@ socket.on('disconnect', function () {
 	console.log('Disconnected');
 });
 socket.on('connect', function(){
-	console.log('Connected');
+	console.log('Connected.');
 	var stream = ss.createStream();
 
 	var imgs = ['test1.jpg', 'test2.jpg'];
 	var i = 0;
 
 	// emit image stream
+	console.log('start emitting...');
 	function emitImage() {
-		console.log('client:emitImage ' + imgs[i]);
+		// console.log('client:emitImage ' + imgs[i]);
 		i = i^1;
 		var stream = ss.createStream();
 		fs.createReadStream(imgs[i]).pipe(stream);
