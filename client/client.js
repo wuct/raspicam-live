c67a577597fb2eb9c5d7b6dd4be0694de8e02d33
wuct <wu.chingting@gmail.com>
2015-01-19 15:04:59 +0800

    stream camera

--- client/client.js
+++ client/client.js
@@ -1,34 +1,6 @@
 var fs = require('fs');
 var ss = require('socket.io-stream');
 
-var camera = new require("raspicam")({
-	mode: 'timelapse',
-	width: 640,
-	height: 640,
-	output: "./temp/image_%06d.jpg",
-	timelapse: 30,
-	timeout: 1000,
-});
-camera.on("start", function (err, timestamp ){
-	console.log("photo started at " + timestamp );
-});
-
-camera.on("read", function (err, timestamp, filename ){
-	console.log("photo image captured with filename: " + filename );
-});
-
-camera.on("exit", function (timestamp ){
-	console.log("photo child process has exited at " + timestamp );
-	camera.stop(function (err, timestamp) {
-		console.log("photo stopped at " + timestamp );
-	})
-});
-camera.on("stop", function (err, timestamp){
-	console.log("timelapse child process has been stopped at " + timestamp);
-});
-
-camera.start();
-
 return;
 var SERVER_URL = process.env.SERVER_URL || 'http://local.host:3000';
 var socket = require('socket.io-client')(SERVER_URL + '/stream', {
@@ -44,21 +16,57 @@ socket.on('disconnect', function () {
 });
 socket.on('connect', function(){
 	console.log('Connected.');
-	var stream = ss.createStream();
 
-	var imgs = ['test1.jpg', 'test2.jpg'];
-	var i = 0;
+	// FOR TESTING
+	// var imgs = ['test1.jpg', 'test2.jpg'];
+	// var i = 0;
 
-	// emit image stream
-	console.log('start emitting...');
-	function emitImage() {
-		// console.log('client:emitImage ' + imgs[i]);
-		i = i^1;
+	// // emit image stream
+	// console.log('start emitting...');
+	// function emitImage() {
+	// 	// console.log('client:emitImage ' + imgs[i]);
+	// 	i = i^1;
+	// 	var stream = ss.createStream();
+	// 	fs.createReadStream(imgs[i]).pipe(stream);
+	// 	ss(socket).emit('client:emitImage', stream, { name: imgs[i] });
+	// }
+
+	// emitImage();
+	// var interval = setInterval(emitImage, 1000);
+
+	// init camera
+	var camera = new require("raspicam")({
+		mode: 'timelapse',
+		width: 640,
+		height: 640,
+		output: "./temp/image.jpg",
+		timelapse: 0,
+		timeout: 0,
+		burst: true,
+		mode: 1
+
+	});
+	camera.on("start", function (err, timestamp ){
+		console.log("photo started at " + timestamp );
+	});
+
+	camera.on("read", function (err, timestamp, filename ){
+		if (/~$/.test(filename)) return;
+		console.log("photo image captured with filename: " + filename );
 		var stream = ss.createStream();
-		fs.createReadStream(imgs[i]).pipe(stream);
+		fs.createReadStream('./temp/' + filename).pipe(stream);
 		ss(socket).emit('client:emitImage', stream, { name: imgs[i] });
-	}
+	});
 
-	emitImage();
-	var interval = setInterval(emitImage, 1000);
+	camera.on("exit", function (timestamp ){
+		console.log("photo child process has exited at " + timestamp );
+		camera.stop(function (err, timestamp) {
+			console.log("photo stopped at " + timestamp );
+		})
+	});
+	camera.on("stop", function (err, timestamp){
+		console.log("timelapse child process has been stopped at " + timestamp);
+	});
+
+	camera.start();
 });
