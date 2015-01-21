76147c3a14d0d379f17fcc2112bdecf2e1d9b519
wuct <wu.chingting@gmail.com>
2015-01-21 23:05:25 +0800

    modified config

--- client/camera.config.js
+++ client/camera.config.js
@@ -1,9 +1,10 @@
+var timelapse = process.argv[2] || 500;
 module.exports = {
 	mode: 'photo',
 	width: 640,
 	height: 640,
 	output: "./temp/image.jpg",
-	timelapse: 100,
+	timelapse: timelapse,
 	timeout: 9999999,
 	quality: 10,
 	burst: true
